772ded4efa950b9a3133475a7310b250c8f922cd
dogukanVaris <varisdogukan@outlook.com>
2024-12-10 20:37:33 +0300

    Added Composition Components

---
+++ Topics/Composition-Components/App.jsx
@@ -0,0 +1,12 @@
+import { GreenSmallButton, RedButton } from "./components/composition";
+
+function App() {
+  return (
+    <>
+      <RedButton text='I am red!' />
+      <GreenSmallButton text='I am small and green!' />
+    </>
+  );
+}
+
+export default App;
